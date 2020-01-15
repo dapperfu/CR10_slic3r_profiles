134a3a1c4862f6080c601e4989dc06374a701426
j <j+make_sandwich@m6700-LINUX>
2020-01-15 17:31:59 -0500

    Startover

--- fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-LINE_LENGTH=80
-PY_VER=py37
-
-pyupgrade --py37-plus --exit-zero-even-if-changed "$1"
-reorder-python-imports --${PY_VER}-plus --exit-zero-even-if-changed "$1"
-black --target-version ${PY_VER} --line-length=${LINE_LENGTH} "$1"
